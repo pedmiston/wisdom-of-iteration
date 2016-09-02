7fa749b0e9e1a5c39b7d90a103866352aa433fe9
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-01 19:33:15 -0500

    Create new app for home page navigation

---
+++ wisdom-app/wisdom/home/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+class HomeTest(TestCase):
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
