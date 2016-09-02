056b760645898e3bf0d8c756970887a47a26745f
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-01 19:54:21 -0500

    Include link to anchoring experiment on home page

--- wisdom-app/wisdom/home/tests.py
+++ wisdom-app/wisdom/home/tests.py
@@ -4,3 +4,7 @@ class HomeTest(TestCase):
     def test_home_page_renders_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_includes_links_to_experiments(self):
+        response = self.client.get('/')
+        self.assertRegexpMatches(response.content.decode(), 'id_anchoring')
