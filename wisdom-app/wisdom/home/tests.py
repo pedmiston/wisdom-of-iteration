bd26b69b41e15551fe182646a255a6ddd4d8b2e8
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-01 20:01:58 -0500

    Create home page template for anchoring experiment

--- wisdom-app/wisdom/home/tests.py
+++ wisdom-app/wisdom/home/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class HomeTest(TestCase):
     def test_home_page_renders_home_template(self):
@@ -8,3 +9,8 @@ class HomeTest(TestCase):
     def test_home_page_includes_links_to_experiments(self):
         response = self.client.get('/')
         self.assertRegexpMatches(response.content.decode(), 'id_anchoring')
+
+    def test_anchoring_page_renders_anchoring_template(self):
+        anchoring_url = reverse('anchoring')
+        response = self.client.get(anchoring_url)
+        self.assertTemplateUsed(response, 'home_pages/anchoring.html')
--- wisdom-app/wisdom/home/views.py
+++ wisdom-app/wisdom/home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def anchoring(request):
+    return render(request, 'home_pages/anchoring.html')
